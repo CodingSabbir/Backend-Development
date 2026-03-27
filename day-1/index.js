48dd79f22f7c1f453a453929afa81ec7d3433a6a
sabbir rahman <mdsabbirrahmanm740@gmail.com>
2026-03-27 10:49:01 -0700

    day-1 update

--- day-1/index.js
+++ day-1/index.js
@@ -1,10 +1,11 @@
 const {addToCart,fullName} = require("./cartModule");
 
 console.log('welcome to BD')
-let number=[10,20,30,40,50,60]
+let number=[10,20,30,40,50,60,90]
 number.forEach((value,number) => {
     console.log(value,number)
 });
+
 
 
 console.log(addToCart())
