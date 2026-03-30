e0edf3ae35ea58bce7909097d2b24012c510355f
sabbir rahman <mdsabbirrahmanm740@gmail.com>
2026-03-30 13:47:05 -0700

    post login api

--- express/index.js
+++ express/index.js
@@ -1,6 +1,9 @@
 let express=require("express")
 
 let app=express()
+app.use(express.json())
+
+
 app.get("/",(req,res)=>{ //http://localhost:3000
 res.send({name:'Kalam', age:54,msg:"home screen"})
 })
@@ -8,6 +11,11 @@ res.send({name:'Kalam', age:54,msg:"home screen"})
 
 app.get("/news",(req,res)=>{ //http://localhost:3000/news
 res.send({name:'Kalam', age:54,msg:"news screen"})
+})
+
+app.post("/login",(req,res)=>{
+    console.log(req.body)
+     res.send({ msg: "Login received", data:res.body })
 })
 
 app.listen("3000")
