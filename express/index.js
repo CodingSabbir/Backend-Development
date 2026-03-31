1212ca9f15915753130a4255af98125310ab6e95
sabbir rahman <mdsabbirrahmanm740@gmail.com>
2026-03-31 10:30:30 -0700

    token and password

--- express/index.js
+++ express/index.js
@@ -2,6 +2,60 @@ let express=require("express")
 
 let app=express()
 app.use(express.json())
+let myPass="12345"
+
+let checkToken=(req,res,next)=>{
+      console.log(req.query.token)
+    if(req.query.token=="" || req.query.token==undefined){
+        return res.send(
+            {
+                status:0,
+                msg:"please fill the token"
+            }
+        )
+
+        
+    }
+  
+     if(req.query.token!= myPass){
+        return res.send(
+            {
+                status:0,
+                msg:"please fill the correct token"
+            }
+        )
+
+        
+    }
+    next()
+}
+app.use(checkToken) // Middleware
+
+app.use((req,res,next)=>{
+if(req.query.pass=="" || req.query.pass==undefined){
+        return res.send(
+            {
+                status:0,
+                msg:"please fill the password"
+            }
+        )
+
+     
+    }
+  
+     if(req.query.pass!= myPass){
+        return res.send(
+            {
+                status:0,
+                msg:"please fill the correct password"
+            }
+        )
+
+        
+    }
+  next()
+})
+
 
 
 app.get("/",(req,res)=>{ //http://localhost:3000
