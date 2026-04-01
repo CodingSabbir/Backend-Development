4b21f54911b3496386c314d29d305004043fd3b7
sabbir rahman <mdsabbirrahmanm740@gmail.com>
2026-04-01 11:02:34 -0700

    .env file seat

--- express/index.js
+++ express/index.js
@@ -1,60 +1,66 @@
 let express=require("express")
+const checkToken = require("./checkTokenMiddleware")
+require('dotenv').config()
 
 let app=express()
+console.log(process.env.myToken)
 app.use(express.json())
-let myPass="12345"
 
-let checkToken=(req,res,next)=>{
-      console.log(req.query.token)
-    if(req.query.token=="" || req.query.token==undefined){
-        return res.send(
-            {
-                status:0,
-                msg:"please fill the token"
-            }
-        )
+
+
+// let myPass="12345"
+
+// let checkToken=(req,res,next)=>{
+//       console.log(req.query.token)
+//     if(req.query.token=="" || req.query.token==undefined){
+//         return res.send(
+//             {
+//                 status:0,
+//                 msg:"please fill the token"
+//             }
+//         )
 
         
-    }
+//     }
   
-     if(req.query.token!= myPass){
-        return res.send(
-            {
-                status:0,
-                msg:"please fill the correct token"
-            }
-        )
+//      if(req.query.token!= myPass){
+//         return res.send(
+//             {
+//                 status:0,
+//                 msg:"please fill the correct token"
+//             }
+//         )
 
         
-    }
-    next()
-}
-app.use(checkToken) // Middleware
+//     }
+//     next()
+// }
+// app.use(checkToken) // Middleware
 
-app.use((req,res,next)=>{
-if(req.query.pass=="" || req.query.pass==undefined){
-        return res.send(
-            {
-                status:0,
-                msg:"please fill the password"
-            }
-        )
+// app.use((req,res,next)=>{
+// if(req.query.pass=="" || req.query.pass==undefined){
+//         return res.send(
+//             {
+//                 status:0,
+//                 msg:"please fill the password"
+//             }
+//         )
 
      
-    }
+//     }
   
-     if(req.query.pass!= myPass){
-        return res.send(
-            {
-                status:0,
-                msg:"please fill the correct password"
-            }
-        )
+//      if(req.query.pass!= myPass){
+//         return res.send(
+//             {
+//                 status:0,
+//                 msg:"please fill the correct password"
+//             }
+//         )
 
         
-    }
-  next()
-})
+//     }
+//   next()
+// })
 
 
 
@@ -63,7 +69,7 @@ res.send({name:'Kalam', age:54,msg:"home screen"})
 })
 
 
-app.get("/news",(req,res)=>{ //http://localhost:3000/news
+app.get("/news", checkToken,(req,res)=>{ //http://localhost:3000/news
 res.send({name:'Kalam', age:54,msg:"news screen"})
 })
 
@@ -72,4 +78,4 @@ app.post("/login",(req,res)=>{
      res.send({ msg: "Login received", data:res.body })
 })
 
-app.listen("3000")
+app.listen(process.env.PORT || 5000)
